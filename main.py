2bb5a0baf70d25c4d105258878c2c0a78e15140a
MrGio7 <lashagiorgi1233@gmail.com>
2021-02-01 15:43:40 +0400

    added blank screen turtle

---
+++ main.py
@@ -0,0 +1,18 @@
+import turtle
+
+#Screen config
+wn = turtle.Screen()
+wn.title("Pong by G.B.")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+#Objects
+
+
+
+
+
+
+while True:
+    wn.update()
